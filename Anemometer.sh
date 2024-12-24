9b4d2835fb18e6df6bb3aaf83e7fb4b4b2003c92
harimoto-1732 <108454293+harimoto-1732@users.noreply.github.com>
2024-12-24 10:15:04 +0900

    Anemometer.shを作成
    風速計Webユニットからcsvファイルを取得する

---
+++ Anemometer.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+head="/var/www/html/wind/data/"
+if [ $(date '+%H%M') = '0001' ]; then
+    url="http://192.168.62.100/csv_download.php?type=ten_min&date="$(date '+%Y/%m/%d' --date 'yesterday')
+    name=$head + $(date '+%Y%m%d' --date 'yesterday').csv
+else
+    url="http://192.168.62.100/csv_download.php?type=ten_min&date="$(date '+%Y/%m/%d')
+    name=$head$(date '+%Y%m%d').csv
+fi
+curl --digest $url | iconv -f SHIFT_JIS -t UTF-8 -o $name
