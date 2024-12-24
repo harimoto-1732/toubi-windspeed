cb1b7079e07d38108529052cf693e705a9e8b500
harimoto-1732 <108454293+harimoto-1732@users.noreply.github.com>
2024-12-24 11:33:26 +0900

    Ubuntuで動作するよう修正

--- Anemometer.sh
+++ Anemometer.sh
@@ -6,6 +6,6 @@ if [ $(date '+%H%M') = '0001' ]; then
     name=$head + $(date '+%Y%m%d' --date 'yesterday').csv
 else
     url="http://192.168.62.100/csv_download.php?type=ten_min&date="$(date '+%Y/%m/%d')
-    name=$head$(date '+%Y%m%d').csv
+    name=$head + $(date '+%Y%m%d').csv
 fi
 curl --digest $url | iconv -f SHIFT_JIS -t UTF-8 -o $name
