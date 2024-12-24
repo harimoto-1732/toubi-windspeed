89f5dae4520171fecb82a36edfd167f8811f948e
harimoto-1732 <108454293+harimoto-1732@users.noreply.github.com>
2024-12-24 14:41:59 +0900

    文字化け対策

--- Anemometer.sh
+++ Anemometer.sh
@@ -3,9 +3,9 @@
 head="/var/www/html/wind/data/"
 if [ $(date '+%H%M') = '0000' ]; then
     url="http://192.168.62.100/csv_download.php?type=ten_min&date="$(date '+%Y/%m/%d' --date 'yesterday')
-    name=$head + $(date '+%Y%m%d' --date 'yesterday').csv
+    name=$head$(date '+%Y%m%d' --date 'yesterday').csv
 else
     url="http://192.168.62.100/csv_download.php?type=ten_min&date="$(date '+%Y/%m/%d')
-    name=$head + $(date '+%Y%m%d').csv
+    name=$head$(date '+%Y%m%d').csv
 fi
-curl --digest $url | iconv -f SHIFT_JIS -t UTF-8 -o $name
+curl --digest $url -o $name
